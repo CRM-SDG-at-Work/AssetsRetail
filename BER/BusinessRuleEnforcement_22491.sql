9b631996c7d55ab4d8a3279e367425a6291032d0
CRM-SDG-at-Work <anshul.pundir@businessnext.com>
2024-12-18 17:56:37 +0530

    Delete BER directory

--- BER/BusinessRuleEnforcement_22491.sql
+++
@@ -1,6 +0,0 @@
---Delete FROM dbo.BusinessRuleEnforcement WHERE OwnerID = 716 AND RuleID IN (22491) and KeyID in (6)
-
-
-Update IDlist Set LastId = t.MaxId from  (select OwnerID, max ( RuleID ) as MaxId from dbo.BusinessRuleEnforcement Group by OwnerID) as t Where  IdList.ItemId = 130   and t.OwnerID =IdList.OwnerID ;
-
-Update BusinessRuleEnforcement SET CreatedBy = 1,LastmodifiedBy = 1 Where OwnerId = 716 ;
