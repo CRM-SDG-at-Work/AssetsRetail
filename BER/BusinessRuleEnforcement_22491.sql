669d364ba2d8a7c16ee57974bbdb951522e321d7
devendra yadav <devendra.yadav@businessnext.com>
2025-01-02 14:40:08 +0530

    My UI Commit 1

--- BER/BusinessRuleEnforcement_22491.sql
+++ BER/BusinessRuleEnforcement_22491.sql
@@ -1,5 +1,6 @@
 --Delete FROM dbo.BusinessRuleEnforcement WHERE OwnerID = 716 AND RuleID IN (22491) and KeyID in (6)
 Delete FROM dbo.BusinessRuleEnforcement WHERE OwnerID = 716 AND RuleID IN (22491) and KeyID in (6)
+Delete FROM dbo.BusinessRuleEnforcement WHERE OwnerID = 716 AND RuleID IN (22491) and KeyID in (6)
 
 
 Update IDlist Set LastId = t.MaxId from  (select OwnerID, max ( RuleID ) as MaxId from dbo.BusinessRuleEnforcement Group by OwnerID) as t Where  IdList.ItemId = 130   and t.OwnerID =IdList.OwnerID ;
